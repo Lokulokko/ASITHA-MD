88c0e5716d4374910c1f8d56f194a0a7e3f8119f
Lokulokko <lokulokko61@gmail.com>
2024-10-25 15:16:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "szEQRIyD#WM0mE2bZNwpmC4SdM_2Orx9AVe5edTtI_ZI5TOVA_w0",
 MONGODB: process.env.MONGODB || "",//enter mongo db url
 };
